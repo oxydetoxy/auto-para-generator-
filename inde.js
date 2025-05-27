91f691193309fd9abda58f28889b5f35bf3831de
oxydetoxy <ishuchoudhary8560@gmail.com>
2025-05-27 13:23:22 +0530

    completed first basic project

--- inde.js
+++ inde.js
@@ -1,11 +1,32 @@
 const input =document.getElementById("numberofwords");
 const container = document.querySelector(".container");
+const generateWord = (n)=>{
+   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+   let text = "";//empty by default
+   for(let i=0;i<n;i++){
+      const randomIndex = (Math.random()*25).toFixed(0);
+text+= letters[randomIndex];
+   }
+   return text ;
+};
+
 let numofwords;
 const generatePara=()=>{
    numofwords= Number(input.value);//access enetered value 
+
 const para = document.createElement("p");
 
-para.innerText = "sample para";
+let data ="";
+for(let i =0;i<numofwords;i++){
+   const randomNumber = (Math.random()*8).toFixed(0);
+   data += generateWord(randomNumber);
+   data += " ";
+
+}
+console.log(data);
+
+para.innerText = data;
      para.setAttribute("class","paras")
  container.append(para);
 };
